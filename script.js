b0bbc1ccdfe39de17efd7009328a9b730532c8a3
Alfredo Zavaleta <azavaleta07@gmail.com>
2021-08-20 18:31:13 -0500

    moved the script sourced link to the bottom of the index.html to correct issue with line 3 in script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+var beginButton = document.getElementById('begin-btn');
+
+beginButton.addEventListener('click',startQuiz)
+
+function startQuiz() {
+    console.log('begin quiz');
+    
+
+}
+
+
+
+function showNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
