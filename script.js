2bf4f7686541a3493452ce41c7d504ab29d24388
Alfredo Zavaleta <azavaleta07@gmail.com>
2021-08-21 18:10:34 -0500

    created var questions that will contain the questions for the quiz. beginning to create a function to randomize the questions for the quiz

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ function startQuiz() {
     console.log('begin quiz');
     beginButton.classList.add('hidden')
     questionContainer.classList.remove('hidden')
-
+    randomQuestion = questions.sort(() => Math.random())
+    currentQuestion = 0
+    showNextQuestion()
 }
 
 var questionContainer = document.getElementById('qa-container')
@@ -20,3 +22,21 @@ function showNextQuestion() {
 function selectAnswer() {
 
 }
+
+var questions = [
+    {
+        question: 'Question 1?',
+        answers: [
+            {text: 'correct', correct: true},
+            {text: 'not correct', correct: false},
+            {text: 'not correct 2', correct: false},
+            {text: 'not correct 3', correct: false},
+            
+        ]
+    }
+
+]
+
+// Need to make sure questions are radomized for each play through
+
+var randomQuestion, currentQuestions
